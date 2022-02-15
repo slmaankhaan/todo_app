bbb089da7a4ff3c4b18513e5b634c788915511f2
slmaankhaan <slmaan.khaan@outlook.com>
2022-02-15 02:49:18 -0500

    App structured into folders and waitress server package installed

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,15 +1,9 @@
 from pyramid.config import Configurator
-from pyramid.view import view_config
-
-
-@view_config(route_name='list', renderer='list.jinja2')
-def list(request):
-    return dict()
 
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.add_route('list', '/')
+    config.add_route('list', '/hello')
     config.scan()
     return config.make_wsgi_app()
