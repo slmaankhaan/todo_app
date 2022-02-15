b05a9b4befa30660dcd9b3be8c3dca6bc4c679fe
slmaankhaan <slmaan.khaan@outlook.com>
2022-02-15 03:31:11 -0500

    Adding routes and templates to the application

--- todo/__init__.py
+++ todo/__init__.py
@@ -4,6 +4,11 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.add_route('list', '/hello')
+    config.add_route('list', '/')
+    config.add_route('add', '/add')
+    config.add_route('view', '/{id}')
+    config.add_route('edit', '/{id}/edit')
+    config.add_route('delete', '/{id}/delete')
     config.scan()
     return config.make_wsgi_app()
+
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,30 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 
 
-@view_config(route_name='list',
-             renderer='templates/list.jinja2')
+@view_config(route_name='list', renderer='templates/list.jinja2')
 def list(request):
     return dict()
+
+
+@view_config(route_name='add', renderer='templates/add.jinja2')
+def add(request):
+    return dict()
+
+
+@view_config(route_name='view', renderer='templates/view.jinja2')
+def view(request):
+    current = request.matchdict.get('id')
+    return dict(current=current)
+
+
+@view_config(route_name='edit', renderer='templates/edit.jinja2')
+def edit(request):
+    current = request.matchdict.get('id')
+    return dict(current=current)
+
+
+@view_config(route_name='delete')
+def delete(request):
+    url = request.route_url('list')
+    return HTTPFound(url)
