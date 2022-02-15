273056fab0aa0549bcc7e47377e1779204a9e4ad
slmaankhaan <slmaan.khaan@outlook.com>
2022-02-15 13:42:46 -0500

    Jinja2 template control flow implemented

--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,23 @@
-
 from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 from pyramid.view import view_config, notfound_view_config
 
+sample_todos = {
+    '1': dict(id='1', title='Get Milk'),
+    '2': dict(id='2', title='Get Eggs')
+}
 
-class ToDo:
+
+class MySite:
     def __init__(self, request):
         self.request = request
 
     @property
     def current(self):
-        return self.request.matchdict.get('id')
+        todo_id = self.request.matchdict.get('id')
+        todo = sample_todos.get(todo_id)
+        if not todo:
+            raise HTTPNotFound()
+        return todo
 
     @notfound_view_config(renderer='templates/notfound.jinja2')
     def not_found(self):
@@ -17,7 +25,11 @@ class ToDo:
 
     @view_config(route_name='list', renderer='templates/list.jinja2')
     def list(self):
-        return dict()
+        msg = self.request.params.get('msg')
+        return dict(
+            todos=sample_todos.values(),
+            msg=msg
+        )
 
     @view_config(route_name='add', renderer='templates/add.jinja2')
     def add(self):
@@ -25,24 +37,23 @@ class ToDo:
 
     @view_config(route_name='view', renderer='templates/view.jinja2')
     def view(self):
-        if self.current != '1':
-            raise HTTPNotFound()
-        return dict()
+        return dict(todo=self.current)
+
+    @view_config(route_name='edit', renderer='templates/edit.jinja2')
+    def edit(self):
+        return dict(todo=self.current)
 
     @view_config(route_name='edit', renderer='templates/edit.jinja2',
-                 request_method='GET')
-    def edit(self):
-        return dict()
-
-    @view_config(route_name='edit', renderer='templates/edit/edit.jinja2',
-                 request_method='POST', request_param='form.submit')
+                 request_method='POST')
     def edit_handler(self):
         new_title = self.request.params.get('new_title')
-        print('New title', new_title)
-        url = self.request.route_url('list')
+        self.current['title'] = new_title
+        msg = 'new_title: ' + new_title
+        url = self.request.route_url('list', _query={'msg': msg})
         return HTTPFound(url)
 
     @view_config(route_name='delete')
     def delete(self):
         url = self.request.route_url('list')
         return HTTPFound(url)
+
