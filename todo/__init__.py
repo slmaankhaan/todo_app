a104abb8c9ecaf42bf770ace9f3d29cc97a2d718
slmaankhaan <slmaan.khaan@outlook.com>
2022-02-15 19:10:04 -0500

    routes updated in __init__.py and styles implemented using bootstrap and css

--- todo/__init__.py
+++ todo/__init__.py
@@ -4,11 +4,12 @@ from pyramid.config import Configurator
 def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.add_route('list', '/')
-    config.add_route('add', '/add')
-    config.add_route('view', '/{id}')
-    config.add_route('edit', '/{id}/edit')
-    config.add_route('delete', '/{id}/delete')
+    config.add_static_view(name='static', path='todo:static')
+    config.add_route('home', '/')
+    config.add_route('todos_list', '/todos')
+    config.add_route('todos_add', '/todos/add')
+    config.add_route('todos_view', '/todos/{id}')
+    config.add_route('todos_edit', '/todos/{id}/edit')
+    config.add_route('todos_delete', '/todos/{id}/delete')
     config.scan()
     return config.make_wsgi_app()
-
--- todo/views.py
+++ todo/views.py
@@ -7,7 +7,7 @@ sample_todos = {
 }
 
 
-class MySite:
+class ToDo:
     def __init__(self, request):
         self.request = request
 
@@ -19,11 +19,12 @@ class MySite:
             raise HTTPNotFound()
         return todo
 
-    @notfound_view_config(renderer='templates/notfound.jinja2')
-    def not_found(self):
+    @view_config(route_name='home', renderer='templates/home.jinja2')
+    def home(self):
         return dict()
 
-    @view_config(route_name='list', renderer='templates/list.jinja2')
+    @view_config(route_name='todos_list',
+                 renderer='templates/list.jinja2')
     def list(self):
         msg = self.request.params.get('msg')
         return dict(
@@ -31,29 +32,36 @@ class MySite:
             msg=msg
         )
 
-    @view_config(route_name='add', renderer='templates/add.jinja2')
+    @view_config(route_name='todos_add',
+                 renderer='templates/add.jinja2')
     def add(self):
         return dict()
 
-    @view_config(route_name='view', renderer='templates/view.jinja2')
+    @view_config(route_name='todos_view',
+                 renderer='templates/view.jinja2')
     def view(self):
         return dict(todo=self.current)
 
-    @view_config(route_name='edit', renderer='templates/edit.jinja2')
+    @view_config(route_name='todos_edit',
+                 renderer='templates/edit.jinja2')
     def edit(self):
         return dict(todo=self.current)
 
-    @view_config(route_name='edit', renderer='templates/edit.jinja2',
+    @view_config(route_name='todos_edit',
+                 renderer='templates/edit.jinja2',
                  request_method='POST')
     def edit_handler(self):
         new_title = self.request.params.get('new_title')
         self.current['title'] = new_title
         msg = 'new_title: ' + new_title
-        url = self.request.route_url('list', _query={'msg': msg})
+        url = self.request.route_url('todos_list', _query={'msg': msg})
         return HTTPFound(url)
 
-    @view_config(route_name='delete')
+    @view_config(route_name='todos_delete')
     def delete(self):
-        url = self.request.route_url('list')
+        url = self.request.route_url('todos_list')
         return HTTPFound(url)
 
+    @notfound_view_config(renderer='templates/notfound.jinja2')
+    def not_found(self):
+        return dict()
